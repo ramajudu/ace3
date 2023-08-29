d7ed0c4296f69c5a169ef3f53e319e3b1fd67ab3
ramajudu <ramajudu@gmail.com>
2023-08-29 16:03:08 +1000

    Update acevariables.tf

--- acevariables.tf
+++ acevariables.tf
@@ -3,6 +3,46 @@ variable "aws_region" {
   default = "ap-southeast-2"
 }
 
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+count= 3
+
+  tags = {
+    Name = "ram_terraform"
+  }
+}
+
+resource "aws_security_group" "sgname"{
+        name        = "rama terraform"
+        description =  "lets see its going to connect through ssh"
+        vpc_id      =  aws_vpc.my_vpc.id
+        count=3
+
+        ingress{
+          description = "ssh"
+         from_port = 22 
+         to_port    = 22
+         protocol   = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+        }
+         ingress{
+           description = "Http"
+         from_port = 8080
+         to_port   = 8080
+         protocol = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+
+       }
+        egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+}
  ## this is to specify the cidr block range 
    variable "aws_cidr_block"{
      description = "CIDR  block for vpc"
